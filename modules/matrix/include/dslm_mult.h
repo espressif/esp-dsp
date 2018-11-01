3e42d50d7abd6bf810f00b8074ebb2d6e9b12903
Dmitry Yakovlev <Dmitry@espressif.com>
2018-11-01 13:43:44 +0300

    Mutrix multiplication functions: 3x3x1,3 4x4x1,4 added.

--- modules/matrix/include/dslm_mult.h
+++ modules/matrix/include/dslm_mult.h
@@ -5,6 +5,10 @@
 
 esp_err_t dslm_mult_32f_ansi(float* A, float* B, float* C, int m, int n, int k);
 esp_err_t dslm_mult_32f_ae32(float* A, float* B, float* C, int m, int n, int k);
+esp_err_t dslm_mult_3x3x3_32f_ae32(float* A, float* B, float* C);
+esp_err_t dslm_mult_3x3x1_32f_ae32(float* A, float* B, float* C);
+esp_err_t dslm_mult_4x4x1_32f_ae32(float* A, float* B, float* C);
+esp_err_t dslm_mult_4x4x4_32f_ae32(float* A, float* B, float* C);
 
 esp_err_t dslm_mult_16s_ansi(int16_t* A, int16_t* B, int16_t* C, int m, int n, int k, int shift);
 esp_err_t dslm_mult_16s_ae32(int16_t* A, int16_t* B, int16_t* C, int m, int n, int k, int shift);
