1bd3ed702424c9ea00bb0e85cd9b499ba2b8a9cc
Dmitry Yakovlev <Dmitry@espressif.com>
2018-11-01 13:51:09 +0300

    Copyright headers added.

--- modules/matrix/include/dslm_mult.h
+++ modules/matrix/include/dslm_mult.h
@@ -1,3 +1,17 @@
+// Copyright 2018-2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. 
+
 #ifndef _dslm_mult_H_
 #define _dslm_mult_H_
 
@@ -5,8 +19,8 @@
 
 esp_err_t dslm_mult_32f_ansi(float* A, float* B, float* C, int m, int n, int k);
 esp_err_t dslm_mult_32f_ae32(float* A, float* B, float* C, int m, int n, int k);
-esp_err_t dslm_mult_3x3x3_32f_ae32(float* A, float* B, float* C);
 esp_err_t dslm_mult_3x3x1_32f_ae32(float* A, float* B, float* C);
+esp_err_t dslm_mult_3x3x3_32f_ae32(float* A, float* B, float* C);
 esp_err_t dslm_mult_4x4x1_32f_ae32(float* A, float* B, float* C);
 esp_err_t dslm_mult_4x4x4_32f_ae32(float* A, float* B, float* C);
 
