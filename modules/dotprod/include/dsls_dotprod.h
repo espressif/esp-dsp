77a26087d85221ae225c79c4f54bb15de368bd19
Dmitry Yakovlev <Dmitry@espressif.com>
2018-11-02 10:05:19 +0300

    Doxygen description added for the dotproduct and matrix multiplicatoin.

--- modules/dotprod/include/dsls_dotprod.h
+++ modules/dotprod/include/dsls_dotprod.h
@@ -24,13 +24,107 @@ extern "C"
 #endif
 // These functions calculates dotproduct of two vectors.
 
-esp_err_t dsls_dotprod_16s_ae32(int16_t* src1, int16_t* src2, int16_t* dest, int len, int8_t shift);
+/**
+ * @function dsls_dotprod_16s_ae32
+ * Dot product calculation for two signed 16 bit arrays: *dest += (src1[i] * src2[i]) >> (15-shift); i= [0..N)
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @param shift shift of the result. 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_dotprod_16s_ansi(int16_t* src1, int16_t* src2, int16_t* dest, int len, int8_t shift);
+
+/**
+ * @function dsls_dotprod_16s_ae32
+ * Dot product calculation for two signed 16 bit arrays: *dest += (src1[i] * src2[i]) >> (15-shift); i= [0..N)
+ * The implementation optimized for Esp32 platform
+ *
+ * @note this function will work correct when length is > 4. If application requires less then 5 please use generic a function dsls_dotprod_16s_ansi.
+ * 
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @param shift shift of the result. 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
+esp_err_t dsls_dotprod_16s_ae32(int16_t* src1, int16_t* src2, int16_t* dest, int len, int8_t shift);
+
 esp_err_t dsls_dotprod_16sc(int16_t* src1, int16_t* src2, int16_t* dest, int len, int8_t shift);
+
+/**
+ * @function dsls_dotprod_16s_ae32
+ * Dot product calculation for two floating point arrays: *dest += (src1[i] * src2[i]); i= [0..N)
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_dotprod_32f_ansi(float* src1, float* src2, float* dest, int len);
+
+/**
+ * @function dsls_dotprode_32f_ansi
+ * Dot product calculation for two floating point arrays: *dest += (src1[i*step1] * src2[i*step2]); i= [0..N)
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @param step1 step over elements in first array
+ * @param step2 step over elements in second array
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_dotprode_32f_ansi(float* src1, float* src2, float* dest, int len, int step1, int step2);
+
+/**
+ * @function dsls_dotprod_32f_ae32
+ * Dot product calculation for two floating point arrays: *dest += (src1[i] * src2[i]); i= [0..N)
+ * The implementation optimized for Esp32 platform
+ *
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_dotprod_32f_ae32(float* src1, float* src2, float* dest, int len);
+
+/**
+ * @function dsls_dotprode_32f_ae32
+ * Dot product calculation for two floating point arrays: *dest += (src1[i*step1] * src2[i*step2]); i= [0..N)
+ * The implementation optimized for Esp32 platform
+ *
+ * @param src1  source array 1
+ * @param src2  source array 2
+ * @param dest  destination pointer
+ * @param len   length of input arrays
+ * @param step1 step over elements in first array
+ * @param step2 step over elements in second array
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_dotprode_32f_ae32(float* src1, float* src2, float* dest, int len, int step1, int step2);
+
+
 esp_err_t dsls_dotprod_32fc(float* src1, float* src2, float* dest, int len);
 
 //#define dsls_dotprod_16s dsls_dotprod_16s_asm
