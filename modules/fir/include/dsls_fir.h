d67c1328f1bba6d4ecef8298a192c949ba95d130
Dmitry <Dmitry@espressif.com>
2018-11-12 16:01:43 +0300

    Fir filter with decimation for 32f type.

--- modules/fir/include/dsls_fir.h
+++ modules/fir/include/dsls_fir.h
@@ -34,17 +34,101 @@ typedef struct fir_32f_s
     int     d_pos;
 }fir_32f_t;
 
+/**
+ * @function dsls_fir_init_32f
+ * Function initialize structure for 32 bit floating point FIR filter
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param fir: pointer to fir filter structure, that must be preallocated
+ * @param coeffs: array with FIR filter coefficients. Must be length N
+ * @param delay: array for FIR filter delay line. Must be length N
+ * @param N: FIR filter length. Length of coeffs and delay arrays.
+ * 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_fir_init_32f(fir_32f_t* fir, float* coeffs, float* delay, int N);
 
-// d - decimation factor
-// start_pos - initial position for delay sample
+/**
+ * @function dsls_fird_init_32f
+ * Function initialize structure for 32 bit floating point FIR filter with decimation
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param fir: pointer to fir filter structure, that must be preallocated
+ * @param coeffs: array with FIR filter coefficients. Must be length N
+ * @param delay: array for FIR filter delay line. Must be length N
+ * @param N: FIR filter length. Length of coeffs and delay arrays.
+ * @param d: decimation factor.
+ * @param start_pos: initial value of decimation counter. Must be [0..d)
+ * 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_fird_init_32f(fir_32f_t* fir, float* coeffs, float* delay, int N, int d, int start_pos);
 
+
+/**
+ * @function dsls_fir_32f_ansi
+ * Function implements FIR filter 
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param fir: pointer to fir filter structure, that must be initialized before
+ * @param x: input array
+ * @param y: array with result of FIR filter
+ * @param len: length of input and result arrays
+ * 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_fir_32f_ansi(fir_32f_t* fir, float* x, float* y, int len);
+/**
+ * @function dsls_fir_32f_ansi
+ * Function implements FIR filter 
+ * The implementation optimized for Esp32 platform
+ *
+ * @param fir: pointer to fir filter structure, that must be initialized before
+ * @param x: input array
+ * @param y: array with result of FIR filter
+ * @param len: length of input and result arrays
+ * 
+ * @return
+ *      - ESP_OK on success
+ *      - One of the error codes from DSP library
+ */
 esp_err_t dsls_fir_32f_ae32(fir_32f_t* fir, float* x, float* y, int len);
 
-// return - amount of processed samples
+/**
+ * @function dsls_fir_32f_ansi
+ * Function implements FIR filter with decimation
+ * The implementation use ANSI C and could be compiled and run on any platform
+ *
+ * @param fir: pointer to fir filter structure, that must be initialized before
+ * @param x: input array
+ * @param y: array with result of FIR filter
+ * @param len: length of input and result arrays
+ * 
+ * @return: function returns amount of samples stored to the output array
+ *          depends on the previous state value could be [0..len/decimation]
+ */
 int dsls_fird_32f_ansi(fir_32f_t* fir, float* x, float* y, int len);
+
+/**
+ * @function dsls_fir_32f_ansi
+ * Function implements FIR filter with decimation
+ * The implementation optimized for Esp32 platform
+ *
+ * @param fir: pointer to fir filter structure, that must be initialized before
+ * @param x: input array
+ * @param y: array with result of FIR filter
+ * @param len: length of input and result arrays
+ * 
+ * @return: function returns amount of samples stored to the output array
+ *          depends on the previous state value could be [0..len/decimation]
+ */
+int dsls_fird_32f_ae32(fir_32f_t* fir, float* x, float* y, int len);
 
 #ifdef __cplusplus
 }
